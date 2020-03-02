aab0458485f4a7f40f3309c395e401cde0b92faa
jingshenwuran <25132324@qq.com>
2020-03-02 20:47:09 +0800

    添加StMedicines类，蔡茂源

---
+++ src/main/java/com/wnxy/hospital/mims/service/stock/StockService.java
@@ -0,0 +1,8 @@
+package com.wnxy.hospital.mims.service.stock;
+
+import com.wnxy.hospital.mims.entity.StIn;
+
+public interface StockService {
+	void insertStock(StIn stIn);
+	
+}
---
+++ src/test/java/com/wnxy/hospital/mims/service/StockServiceTest.java
@@ -0,0 +1,30 @@
+package com.wnxy.hospital.mims.service;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wnxy.hospital.mims.entity.StIn;
+import com.wnxy.hospital.mims.service.stock.StockService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockServiceTest {
+	@Autowired
+	ApplicationContext ac;
+	@Test
+	public void testAdd() {
+		StockService stockService = (StockService) ac.getBean("stockServiceImpl");
+		StIn stIn=new StIn(UUID.randomUUID().toString(),"1","1",new Date());
+		stockService.insertStock(stIn);
+	}
+}
