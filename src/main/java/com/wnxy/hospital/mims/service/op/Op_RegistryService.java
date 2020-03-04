e7b1b6630747f3b5e20c4909195df6dea4b0ad69
ArmWatt <ArmWatt@hotmail.com>
2020-03-05 04:33:27 +0800

    门诊模块，员工表、门诊科室表、部门表、挂号收费等级表CRUD

--- src/main/java/com/wnxy/hospital/mims/service/op/Op_RegistryService.java
+++ src/main/java/com/wnxy/hospital/mims/service/op/Op_RegistryService.java
@@ -3,12 +3,13 @@ package com.wnxy.hospital.mims.service.op;
 import java.util.List;
 
 import com.wnxy.hospital.mims.entity.OpCard;
+import com.wnxy.hospital.mims.entity.OpDoclevel;
 import com.wnxy.hospital.mims.entity.OpPatientinfo;
 import com.wnxy.hospital.mims.entity.OpRegistry;
 
 public interface Op_RegistryService {// 新办就诊卡，挂号
 
-	public void newCard(String pt_id) throws RuntimeException;// 新办就诊卡
+	public OpCard newCard(String pt_id) throws RuntimeException;// 新办就诊卡
 
 	public void rebondCard(OpCard opCard);// 挂失原就诊卡，将身份证和就诊卡号绑定关系解除，将对应卡号状态改为不可用
 
@@ -16,11 +17,16 @@ public interface Op_RegistryService {// 新办就诊卡，挂号
 
 	public OpPatientinfo queryPatientBypt_id(String pt_id);// 根据病人身份证号查病人基本信息
 
+	List<OpCard> queryCardBypt_id(String pt_id);// 根据病人身份证号查就诊卡
 
-	public void newOpRegistry(OpRegistry opRegistry);// 新建挂号单
+	List<OpCard> queryCardBycardIdAndpt_id(String cardId, String pt_id);// 根据卡号和身份证号查就诊卡
 
-	List<OpCard> queryCardBypt_id(String pt_id);//根据病人身份证号查就诊卡
+	List<OpCard> queryCardBycardId(String cardId);// 根据就诊卡号查有效就诊卡
 
-	List<OpCard> queryCardBycardIdAndpt_id(String cardId, String pt_id);//根据卡号和身份证号查就诊卡
+	public List<OpDoclevel> queryOpDoclevelByLevel(Integer level);// 根据医生等级查医生编号和收费标准
+	
+	public List<Integer> queryAllOpDoclevel();//查询所有医生等级
+
+	OpRegistry newOpRegistry(String dlId, String empId, String cardId);//新建挂号单
 
 }
--- src/test/java/com/wnxy/hospital/mims/service/OpServiceTest.java
+++ src/test/java/com/wnxy/hospital/mims/service/OpServiceTest.java
@@ -1,7 +1,7 @@
 package com.wnxy.hospital.mims.service;
 
+import java.math.BigDecimal;
 import java.util.Date;
-import java.util.UUID;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.thymeleaf.expression.Calendars;
 
+import com.wnxy.hospital.mims.entity.Emp;
+import com.wnxy.hospital.mims.entity.Office;
 import com.wnxy.hospital.mims.entity.OpCard;
+import com.wnxy.hospital.mims.entity.OpDep;
+import com.wnxy.hospital.mims.entity.OpDoclevel;
 import com.wnxy.hospital.mims.entity.OpPatientinfo;
-import com.wnxy.hospital.mims.service.op.Op_RegistryService;
+import com.wnxy.hospital.mims.service.op.impl.Op_InfoManagementServiceImpl;
 import com.wnxy.hospital.mims.service.op.impl.Op_RegistryServiceImpl;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,8 +31,8 @@ public class OpServiceTest {
 	@Test
 	public void test01() {// 录入病人基本信息
 		Op_RegistryServiceImpl op_RegistryServiceImpl = (Op_RegistryServiceImpl) ac.getBean("op_RegistryServiceImpl");
-		OpPatientinfo opPatientinfo = new OpPatientinfo("513411111111111111", "zs",11, "男",
-				new Date(19111111), new Date(), "13888888888", "ls", "13999999999", "chenedu");
+		OpPatientinfo opPatientinfo = new OpPatientinfo("513411111111111111", "zs", 11, "男", new Date(19111111),
+				new Date(), "13888888888", "ls", "13999999999", "chenedu");
 		op_RegistryServiceImpl.newPatientInfo(opPatientinfo);
 
 	}
@@ -57,4 +60,116 @@ public class OpServiceTest {
 		System.out.println(patientInfo);
 	}
 
+	@Test
+	public void test05() {// 添加部门信息
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		op_InfoManagementServiceImpl.addOffice("门诊部");
+
+	}
+
+	@Test
+	public void test051() {// 查所有部门
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		System.out.println(op_InfoManagementServiceImpl.queryAllOffice());
+
+	}
+
+	@Test
+	public void test052() {// 查指定编号的部门
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		System.out.println(op_InfoManagementServiceImpl.queryOfficeByOfficeId("7e711304f3ae48d8b33b309b4ae0613a"));
+
+	}
+
+	@Test
+	public void test06() {// 添加科室信息
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		op_InfoManagementServiceImpl.addOpDep("内科");
+
+	}
+
+	@Test
+	public void test061() {// 查所有科室
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		System.out.println(op_InfoManagementServiceImpl.queryAllOpDep());
+
+	}
+
+	@Test
+	public void test062() {// 查指定编号的科室
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		System.out.println(op_InfoManagementServiceImpl.queryOpDepByDepId("31beb833020e49f2918df3d5d986b90b"));
+
+	}
+
+	@Test
+	public void test07() {// 添加员工
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		String officeId = "319c8a544fbf4b45a1c4c7ae3e4fb29d";// 部门编号
+		Office office = null;// 部门域属性
+		String depId = "31beb833020e49f2918df3d5d986b90b";// 科室编号
+		OpDep opDep = null;// 科室域属性
+		String empName = "zs";// 员工名字
+		String empIdentity = "513411111111111114";// 员工身份证号
+		String empSex = "男";// 员工性别
+		String empAddress = "chenadu";// 员工住址
+		String empEmail = "1111@kjdf.com";// 员工电邮
+
+		Date empBirth = new Date("1999/09/09");// 员工生日
+
+		String empNation = "CN";// 员工国籍
+
+		Date empHireday = new Date();// 员工入职时间
+
+		String empPhoto = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583351222492&di=8f5db222937e7c70c522143d7b99aafd&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F9c16fdfaaf51f3de81a9ddd89feef01f3a297976.jpg";// 员工证件照
+
+		String empPhone = "13999999999";// 员工电话号
+
+		Emp tempEmp = new Emp(null, officeId, office, depId, opDep, empName, empIdentity, empSex, empAddress, empEmail,
+				empBirth, empNation, empHireday, empPhoto, empPhone);
+		op_InfoManagementServiceImpl.addEmp(tempEmp);
+
+	}
+
+	@Test
+	public void test071() {// 查所有员工
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		System.out.println(op_InfoManagementServiceImpl.queryAllEmp());
+
+	}
+
+	@Test
+	public void test072() {// 查指定科室编号的员工
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		System.out.println(op_InfoManagementServiceImpl.queryEmpByDepId("31beb833020e49f2918df3d5d986b90b"));
+
+	}
+
+	@Test
+	public void test08() {// 添加医生收费等级记录
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		String dlId = null;
+		String empId = "f96f2fb706e4452cb038806763b2af2c";
+		Integer level = 2;
+		float price = 10.00f;
+		OpDoclevel tempOpDoclevel = new OpDoclevel(dlId, empId, level, price);
+		op_InfoManagementServiceImpl.addOpDoclevel(tempOpDoclevel);
+	}
+
+	@Test
+	public void test081() {// 查所有收费等级，理论上要去重，同一个等级只显示一种
+		Op_InfoManagementServiceImpl op_InfoManagementServiceImpl = (Op_InfoManagementServiceImpl) ac
+				.getBean("op_InfoManagementServiceImpl");
+		System.out.println(op_InfoManagementServiceImpl.queryAllLevel());
+	}
 }
