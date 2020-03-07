42dc04025eb522f706cc70dc9107ae5c384998d2
leon <76850211@qq.com>
2020-03-08 00:28:55 +0800

    药房修改药品信息分页

--- src/main/java/com/wnxy/hospital/mims/service/ph/PhMedicinesService.java
+++ src/main/java/com/wnxy/hospital/mims/service/ph/PhMedicinesService.java
@@ -3,14 +3,18 @@ package com.wnxy.hospital.mims.service.ph;
 import java.util.List;
 
 import com.wnxy.hospital.mims.entity.PhMedicines;
+import com.wnxy.hospital.mims.entity.ph.page.PhPageBean;
 
 public interface PhMedicinesService {
 	//根据药品id查询药品信息
 	PhMedicines getMedicineById(String medicineId);
 	//根据条件查询药品信息
-	List<PhMedicines> getMedicinesByCondition(PhMedicines pm);
+	PhPageBean<PhMedicines> getMedicinesByCondition(PhMedicines pm, int pageIndex, int pageSize);
 	//修改药品信息
 	int editMedicineByCondition(PhMedicines pm);
 	//根据药品名称查询药品信息
 	PhMedicines getMedicineByName(String medicinesName);
+	//初始化药品信息页面，查询全部信息
+	PhPageBean<PhMedicines> getAllMedicine(int pageIndex, int pageSize);
+	
 }
