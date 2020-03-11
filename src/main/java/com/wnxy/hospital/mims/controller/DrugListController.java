60ce5162960ba5a09f7f3323143b242e00f4aea9
para-paradise <981405195@qq.com>
2020-03-11 09:03:42 +0800

    药品对接

--- src/main/java/com/wnxy/hospital/mims/controller/DrugListController.java
+++
@@ -1,8 +0,0 @@
-package com.wnxy.hospital.mims.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class DrugListController {
-
-}
--- src/main/java/com/wnxy/hospital/mims/controller/TreatmentController.java
+++
@@ -1,5 +0,0 @@
-package com.wnxy.hospital.mims.controller;
-
-public class TreatmentController {
-
-}
